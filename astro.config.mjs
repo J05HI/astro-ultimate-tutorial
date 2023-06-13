487dc0a946c621e6108291690d5623412bdb605b
Joshua Ott <joshuaott98@gmail.com>
2023-06-13 20:20:08 +0200

    Remove unused stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config'
 import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
 import tailwind from '@astrojs/tailwind'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
